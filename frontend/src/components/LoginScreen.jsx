dfdaaa3b79b317441d1ce11efdfcf00e64c63435
ijon9 <isaac.jon72@gmail.com>
2023-03-18 21:47:29 -0400

    Login

--- frontend/src/components/LoginScreen.jsx
+++ frontend/src/components/LoginScreen.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { GoogleLogin } from '@react-oauth/google';
 
 function LoginScreen() {
-  return <div>LoginScreen</div>;
+  return (
+    <>
+    <div>LoginScreen</div>
+    <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {console.log('Login Failed');}}/>
+    </>
+  );
+  
 }
 
 export default LoginScreen;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style/index.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="388672686786-fvfgrg7p3484ckh557liclfthd0jecv8.apps.googleusercontent.com"> <App /> </GoogleOAuthProvider>;
   </React.StrictMode>
 );
