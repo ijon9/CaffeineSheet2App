cd61c6d6daf98abcdf801c9ffecbcdede24f56f5
Jason Wu <jsonwu34@gmail.com>
2023-03-18 22:09:08 -0400

    redirect after google login

--- frontend/src/components/LoginScreen.jsx
+++ frontend/src/components/LoginScreen.jsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function LoginScreen() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  function handleSuccess(response) {
+    setIsLoggedIn(true);
+    // Redirect to the homepage or any other page after successful login
+    navigate("/yourapps");
+  }
   return (
     <>
-    <div>LoginScreen</div>
-    <GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {console.log('Login Failed');}}/>
+      <div>LoginScreen</div>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
     </>
   );
-  
 }
 
 export default LoginScreen;
