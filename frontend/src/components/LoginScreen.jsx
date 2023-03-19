58d946d3394244e2f11cfc053508f41191b0e011
ijon9 <isaac.jon72@gmail.com>
2023-03-18 23:34:07 -0400

    decoding token

--- frontend/src/components/LoginScreen.jsx
+++ frontend/src/components/LoginScreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import jwt_decode from "jwt-decode";
 
 function LoginScreen() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -9,6 +10,9 @@ function LoginScreen() {
   // credentialResponse
   function handleSuccess(response) {
     setIsLoggedIn(true);
+    var decoded = jwt_decode(response.credential)
+    console.log(decoded.email)
+    console.log(decoded.family_name)
     // Redirect to the homepage or any other page after successful login
     navigate("/yourapps");
   }
